4b434b9c69379ce48eb0452203b6fba085d69d87
Amir Shah <axs@tutamail.com>
2020-08-09 18:57:17 +0100

    Added unit test for returning html

--- blog/tests.py
+++ blog/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+
 from blog.views import post_list
 
 # Create your tests here.
@@ -16,3 +18,12 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, post_list)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = post_list(request)
+        html = response.content.decode('utf8')
+        # print(html)
+        self.assertTrue(html.startswith('\n'))
+        self.assertIn('<title>My Blog</title>', html)
+        self.assertTrue(html.endswith('</html>'))
