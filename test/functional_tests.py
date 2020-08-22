a2d407a248dc2df808bf536b282d479581af2913
Amir Shah <axs@tutamail.com>
2020-08-22 15:31:06 +0100

    Added navbar

--- test/functional_tests.py
+++ test/functional_tests.py
@@ -17,6 +17,11 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('My Blog', self.browser.title)
         # self.fail('Finish the test!')
 
+        first_post_title = self.browser.find_element_by_tag_name('h2').text
+        self.assertIn('1st post', first_post_title)
+        first_post_text = self.find_element_by_tag_name('p').text
+        self.assertIn('Is it working?', first_post_text)
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
